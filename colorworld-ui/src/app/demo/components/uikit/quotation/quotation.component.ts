474fd2e788ae21828cf20dff31d625a2fa964c1c
TIAA user <TIAA user@TIAABCP-71XFPX3>
2024-03-08 07:31:22 +0530

    Scroll to the Top added

--- colorworld-ui/src/app/demo/components/uikit/quotation/quotation.component.ts
+++ colorworld-ui/src/app/demo/components/uikit/quotation/quotation.component.ts
@@ -90,6 +90,16 @@ export class QuotationComponent implements OnInit, AfterViewChecked  {
         }
     }
 
+    scrollToTop() {
+        (function smoothscroll() {
+            var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+            if (currentScroll > 0) {
+                window.requestAnimationFrame(smoothscroll);
+                window.scrollTo(0, currentScroll - (currentScroll / 8));
+            }
+        })();
+    }
+
     dummy() {
         this.visible = true;
         this.activeItem = this.items[1];     
--- colorworld-ui/src/app/demo/components/uikit/tax-invoice/tax-invoice.component.ts
+++ colorworld-ui/src/app/demo/components/uikit/tax-invoice/tax-invoice.component.ts
@@ -89,6 +89,16 @@ export class TaxInvoiceComponent implements OnInit, AfterViewChecked {
         }
     }
 
+    scrollToTop() {
+        (function smoothscroll() {
+            var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+            if (currentScroll > 0) {
+                window.requestAnimationFrame(smoothscroll);
+                window.scrollTo(0, currentScroll - (currentScroll / 8));
+            }
+        })();
+    }
+
     dummy() {
         this.visible = true;
         this.activeItem = this.items[1];     
